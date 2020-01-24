dffd25b91dbe47e68b46c74b052f8de7f7d3adf9
Frank Faustino <nawzyah@gmail.com>
2020-01-24 15:17:21 -0800

    ⚡️ Update Apollo context to include data from cookies
    - Add cors options

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,35 @@
-import * as dotenv from 'dotenv'
-dotenv.config({ path: './.env' })
+require('dotenv').config({ path: './.env' })
 import { ApolloServer, ServerInfo } from 'apollo-server'
+import jwt from 'jsonwebtoken'
 
 import schema from './modules'
+import { DecodedToken } from './lib'
+
+const { CLIENT, JWT_SECRET, PORT } = process.env
+
+const cors = {
+  credentials: true,
+  origin: [CLIENT || 'http://localhost:3000'],
+}
+
+const context = ({ req, res }) => {
+  console.log('🔥 Request header: ', req.headers)
+  const { cookie } = req.headers
+  const returnVal = { req, res }
+  if (cookie) {
+    const decoded = jwt.verify(cookie.split('=')[1], JWT_SECRET!)
+    const { username, sessionId } = decoded as DecodedToken
+    Object.assign(returnVal, { username, sessionId })
+  }
+  return returnVal
+}
 
 const server = new ApolloServer({
-  context: ({ req, res }) => {
-    // TO-DO: Add mariadb to ctx
-    console.log('🔥 Request header: ', req.headers)
-    return { req, res }
-  },
-  cors: {
-    origin: ['http://localhost:3000', 'http://localhost:4000'],
-    credentials: true
-  },
-  schema
+  context,
+  cors,
+  schema,
 })
 
 server
-  .listen({ port: process.env.PORT || 4000 })
+  .listen({ port: PORT || 4000 })
   .then(({ url }: ServerInfo) => console.log(`🤖 Server ready at ${url}`))
