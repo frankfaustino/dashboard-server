070fd8e68b93e5b956b161cfdf83eb2c789d74f1
Frank Faustino <nawzyah@gmail.com>
2020-01-24 15:11:28 -0800

    ✨ Add queryUser
    - Change session_count datatype to DEFAULT(0)
    - Change NVARCHAR to VARCHAR to support emojis 😎
    - Add roles table

--- src/db/fns.ts
+++ src/db/fns.ts
@@ -19,6 +19,29 @@ export async function query(queries: Array<string>): Promise<any> {
   }
 }
 
+export async function queryUser(user?: string) {
+  try {
+    const response = await query([`SELECT * FROM user WHERE username = "${user}"`])
+
+    if (response[0][0]) {
+      const { id, name, username, email, token, session_count } = response[0][0]
+
+      return {
+        id,
+        name,
+        username,
+        email,
+        token,
+        sessionCount: session_count
+      }
+    }
+    return null
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
+
 export async function showTables(): Promise<Array<string>> {
   const result = await query(['SHOW TABLES;'])
   console.log('showTables: ', result)
@@ -30,6 +53,7 @@ export async function createTable(tableName: string, columns: Array<string>) {
     console.log(`✍️  Creating ${tableName} table...`)
     const queryList = [`DROP TABLE IF EXISTS ${tableName};`, `CREATE TABLE ${tableName} (${columns}) CHARACTER SET "utf8mb4" ;`, 'SHOW TABLES;']
     const tables = await query(queryList)
+    console.log(tables)
     return tables
   } catch (e) {
     console.error(e)
@@ -48,7 +72,7 @@ export async function setUpTables() {
       'username NVARCHAR(255)',
       'email NVARCHAR(255)',
       'token NVARCHAR(255)',
-      'session_count INT',
+      'session_count INT DEFAULT(0)',
       'photo BLOB'
     ]
     await createTable('user', fields)
@@ -66,10 +90,10 @@ export async function setUpTables() {
       'author NVARCHAR(255)',
       'created_at DATETIME',
       'updated_at DATETIME',
-      'title NVARCHAR(255)',
-      'description NVARCHAR(4000)',
-      'link VARCHAR(255)',
-      'type VARCHAR(255)'
+      'title VARCHAR(255)',
+      'description VARCHAR(4000)',
+      'link NVARCHAR(255)',
+      'type NVARCHAR(255)'
     ]
     await createTable('news', fields)
   }
@@ -79,7 +103,7 @@ export async function setUpTables() {
       'author_id INT',
       'author NVARCHAR(255)',
       'created_at DATETIME DEFAULT CURRENT_TIMESTAMP',
-      'text NVARCHAR(255)',
+      'text VARCHAR(255)',
       'is_urgent BOOLEAN'
     ]
     await createTable('announcements', fields)
@@ -88,6 +112,20 @@ export async function setUpTables() {
     await createTable('user_announcement', [
       'user_id INT NOT NULL',
       'announcement_id INT PRIMARY KEY'
+    ])
+  }
+  if (!tables.includes('roles')) {
+    await createTable('roles', [
+      'id INT NOT NULL AUTO_INCREMENT PRIMARY KEY',
+      'title NVARCHAR(255)'
+    ])
+  }
+  if (!tables.includes('user_roles')) {
+    await createTable('user_roles', [
+      'user_id INT',
+      'role_id INT',
+      'FOREIGN KEY (user_id) REFERENCES user(id)',
+      'FOREIGN KEY (role_id) REFERENCES roles(id)'
     ])
   }
 
